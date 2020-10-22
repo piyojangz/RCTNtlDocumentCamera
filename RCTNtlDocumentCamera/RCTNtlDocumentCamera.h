124dfd2a4ecf4fc4c3e092a6670ecc3d986ae3ab
Breeshy Sama <breesh.comsci@gmail.com>
2020-10-22 16:50:43 +0700

    Initial Commit

---
+++ RCTNtlDocumentCamera/RCTNtlDocumentCamera.h
@@ -0,0 +1,12 @@
+//
+//  RCTNtlDocumentCamera.h
+//  RCTNtlDocumentCamera
+//
+//  Created by Breeshy Sama on 22/10/2563 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTNtlDocumentCamera : NSObject
+
+@end
---
+++ RCTNtlDocumentCamera/RCTNtlDocumentCamera.m
@@ -0,0 +1,12 @@
+//
+//  RCTNtlDocumentCamera.m
+//  RCTNtlDocumentCamera
+//
+//  Created by Breeshy Sama on 22/10/2563 BE.
+//
+
+#import "RCTNtlDocumentCamera.h"
+
+@implementation RCTNtlDocumentCamera
+
+@end
